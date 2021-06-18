1c2a32a0c6f1e95f721bdc5b1035a16e8ba75800
kavishamodi97 <kavishamodi971@gmail.com>
2021-06-19 00:30:10 +0530

    Implement Menu on profile icon inside home page

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -3,8 +3,21 @@ import "./Header.css";
 import Input from "@material-ui/core/Input";
 import InputAdornment from "@material-ui/core/InputAdornment";
 import SearchOutlinedIcon from "@material-ui/icons/SearchOutlined";
+import Avatar from "@material-ui/core/Avatar";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
 
 const Header = (props) => {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <div>
       <header>
@@ -24,6 +37,22 @@ const Header = (props) => {
                   </InputAdornment>
                 }
               />
+              <Avatar
+                alt="Remy Sharp"
+                src=""
+                className="icon-large"
+                onClick={handleClick}
+              />
+              <Menu
+                id="simple-menu"
+                anchorEl={anchorEl}
+                keepMounted
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+              >
+                <MenuItem onClick={handleClose}>My account</MenuItem>
+                <MenuItem onClick={handleClose}>Logout</MenuItem>
+              </Menu>
             </div>
           )}
         </div>
