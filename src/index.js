fd5c612e4c0e6ab056b22ab649d74de4d94fc519
kavishamodi97 <kavishamodi971@gmail.com>
2021-06-21 15:20:59 +0530

    Moved Controller.js files into screens folder

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Controller from './common/Controller';
+import Controller from './screens/Controller';
 
 ReactDOM.render(
     <Controller/>,
