39a23d6efe6889dc5a840c76307783049a2f5c0f
kavishamodi97 <kavishamodi971@gmail.com>
2021-06-24 12:23:08 +0530

    Implement Edit Section Inside Profile Section

--- src/screens/profile/Profile.js
+++ src/screens/profile/Profile.js
@@ -6,11 +6,42 @@ import instaLogo from "../../assets/insta.png";
 import Typography from "@material-ui/core/Typography";
 import Fab from '@material-ui/core/Fab';
 import EditIcon from '@material-ui/icons/Edit';
+import Modal from '@material-ui/core/Modal';
+import FormControl from "@material-ui/core/FormControl";
+import InputLabel from "@material-ui/core/InputLabel";
+import Input from "@material-ui/core/Input";
+import Button from "@material-ui/core/Button";
+import FormHelperText from "@material-ui/core/FormHelperText";
+import { withStyles } from "@material-ui/core/styles";
+
+const styles = (theme) => ({
+  formControl: {
+    margin: theme.spacing(),
+    width: 250,
+  },
+  buttonControl: {
+    margin: theme.spacing(),
+    pointer: "cursor",
+  },
+  modal: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  paper: {
+    backgroundColor: theme.palette.background.paper,
+    border: '1px solid #c0c0c0',
+    borderRadius: '4px',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+});
 
 const profileStyle = {
   avatarImageStyle: {
     width: 120,
     height: 120,
+    cursor: "pointer"
   },
   profileInfoStyle: {
     fontSize: "16px",
@@ -25,7 +56,7 @@ const profileStyle = {
     fontSize: "20px",
     fontWeight: "bold",
     paddingTop: "15px"
-  }
+  },
 }
 
 class Profile extends Component {
@@ -33,13 +64,33 @@ class Profile extends Component {
   constructor() {
     super();
     this.state = {
-      postCount: 6,
-      follows: 4,
-      followedBy: 6
+      postCount: 20,
+      follows: 100,
+      followedBy: 120,
+      modalOpen: false,
+      fullname: "",
+      fullnameRequired: "dispNone",
     }
+  }
+  openModelHandler = () => {
+    this.setState({ modalOpen: true })
+  }
+
+  closeModelHandler = () => {
+    this.setState({ modalOpen: false })
+  }
+
+  inputFullNameChangeHandler = (event) => {
+    this.setState({ fullname: event.target.value })
+  }
+
+  updateClickHandler = () => {
+    this.state.fullname === "" ? this.setState({ fullnameRequired: "dispBlock" }) : this.setState({ fullnameRequired: "dispNone" });
+    if (this.state.fullname === "") return;
   }
 
   render() {
+    const { classes } = this.props;
     return (
       <div>
         <Header
@@ -62,8 +113,11 @@ class Profile extends Component {
               <Typography>
                 <span className="profile-text">Posts: {this.state.postCount}</span>
                 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                 <span className="profile-text">Follows: {this.state.follows}</span>
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                 <span className="profile-text">Followed By: {this.state.followedBy}</span>
               </Typography>
             </div>
@@ -75,9 +129,44 @@ class Profile extends Component {
                 </Typography>
               </div>
               <div className="edit-button">
-                <Fab style={profileStyle.editIconStyle} color="secondary" aria-label="edit">
+                <Fab style={profileStyle.editIconStyle} color="secondary" aria-label="edit" onClick={this.openModelHandler}>
                   <EditIcon />
                 </Fab>
+                <Modal
+                  aria-labelledby="transition-modal-title"
+                  aria-describedby="transition-modal-description"
+                  className={classes.modal}
+                  open={this.state.modalOpen}
+                  onClose={this.closeModelHandler}
+                >
+                  <div className={classes.paper}>
+                    <Typography variant="headline" component="h2">
+                      Edit
+                    </Typography>
+                    <br />
+                    <FormControl className={classes.formControl} required>
+                      <InputLabel htmlFor="fullname">Full Name</InputLabel>
+                      <Input
+                        id="fullname"
+                        type="text"
+                        fullname={this.state.fullname}
+                        onChange={this.inputFullNameChangeHandler}
+                      />
+                      <FormHelperText className={this.state.fullnameRequired}>
+                        <span className="red">required</span>
+                      </FormHelperText>
+                    </FormControl>
+                    <br />
+                    <br />
+                    <Button
+                      variant="contained"
+                      color="primary"
+                      onClick={this.updateClickHandler}
+                    >
+                      UPDATE
+                    </Button>
+                  </div>
+                </Modal>
               </div>
             </div>
           </div>
@@ -87,4 +176,4 @@ class Profile extends Component {
   }
 }
 
-export default Profile;
+export default withStyles(styles)(Profile);
